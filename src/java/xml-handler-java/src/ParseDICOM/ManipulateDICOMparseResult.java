382950e2e02799009af47ee63979612843104969
hugeiezzy <hugeiezzy@gmail.com>
2010-10-23 01:53:54 +0100

    MRIW direct convert support.

--- src/java/xml-handler-java/src/ParseDICOM/ManipulateDICOMparseResult.java
+++ src/java/xml-handler-java/src/ParseDICOM/ManipulateDICOMparseResult.java
@@ -713,7 +713,7 @@ public class ManipulateDICOMparseResult {
 	
 	
 	// Write the ROI info to the XML file
-	public void writeToXML(String in_xml_filename, String out_xml_filename, double boundaries_array[][][][], int series_n, String application_name, int roi_dimension) 
+	public static void writeToXML(String in_xml_filename, String out_xml_filename, double boundaries_array[][][][], int series_n, String application_name, int roi_dimension) 
 	throws XMLStreamException, IOException{
 		
 		// Name of the original XML file was given changed as .tmp file
@@ -888,7 +888,7 @@ public class ManipulateDICOMparseResult {
 	}
 	
 	// Write the result of parsing to XML file
-    public void writeToXML(String working_directory,
+    public static void writeToXML(String working_directory,
             String dicom_files[][][], String dicom_metadata[][][], String studyInstanceUID)
     throws XMLStreamException, IOException{
 
@@ -962,7 +962,7 @@ public class ManipulateDICOMparseResult {
     
     
 	// Write the ROI info to the XML file
-	public void writeToXML(String in_xml_filename, String out_xml_filename, double boundaries_array[][][][], int series_n, String application_name, int roi_dimension, String roi_creation_username, String roi_creation_date, String image_size[]) 
+	public static void writeToXML(String in_xml_filename, String out_xml_filename, double boundaries_array[][][][], int series_n, String application_name, int roi_dimension, String roi_creation_username, String roi_creation_date, String image_size[]) 
 	throws XMLStreamException, IOException{
 		
 		// Name of the original XML file was given changed as .tmp file
@@ -1119,6 +1119,213 @@ public class ManipulateDICOMparseResult {
 						
 						xmlw.writeEndElement();
 					}
+										
+					file_element_counter++;
+				}
+				
+				// Any other elements
+				else{
+					xmlw.writeStartElement(start.getName().getLocalPart());	
+					
+					for(Iterator attr_i = start.getAttributes(); attr_i.hasNext(); ){
+						Attribute attr = (Attribute) attr_i.next();
+						xmlw.writeAttribute(attr.getName().getLocalPart(), attr.getValue());
+					}
+				}
+				
+			}else if(readerEvent.isEndElement()){
+				// Write EndElement skipping those elements
+				if(series_n+1 == series_counter && in_roi_element){					
+				}else if(series_n+1 == series_counter && in_roi_2d_element){
+				}else{
+					xmlw.writeEndElement();
+				}
+			}else if(readerEvent.isEndDocument()){
+				xmlw.writeEndDocument();
+			}
+		}
+		
+		reader.close();
+		xmlw.close();
+		
+		/*
+		System.out.println("num_labels: " + Integer.toString( boundaries_array.length ));
+		for(int i=0; i<boundaries_array.length; i++){
+			System.out.println("label: " + Integer.toString(i+1) + " num_files: " + Integer.toString(boundaries_array[i].length));
+			for(int j=0; j<boundaries_array[i].length; j++){
+				System.out.println("file_num: " + j + " " + Integer.toString(boundaries_array[i][j].length));
+				for(int k=0; k<boundaries_array[i][j].length; k++){
+					System.out.println(boundaries_array[i][j][k][0] + " " + boundaries_array[i][j][k][1]);
+				}
+			}
+		}
+		*/
+	}
+	
+	
+	// Write the ROI info to the XML file
+	public static void writeToXML(String in_xml_filename, String out_xml_filename, String xs[], String ys[], int series_n, String application_name, int roi_dimension, String roi_creation_username, String roi_creation_date, String image_size[]) 
+	throws XMLStreamException, IOException{
+		
+		// Name of the original XML file was given changed as .tmp file
+		// and it will be written again with original name with the additional ROI info
+		
+		XMLInputFactory xmlif = XMLInputFactory.newInstance();
+		XMLEventReader reader = xmlif.createXMLEventReader(in_xml_filename, new FileInputStream(in_xml_filename));
+		
+		XMLOutputFactory xmlof = XMLOutputFactory.newInstance();
+		XMLStreamWriter xmlw = xmlof.createXMLStreamWriter(new FileWriter(out_xml_filename));
+				
+		boolean in_roi_element = false;
+		boolean in_roi_2d_element = false;
+		int file_element_counter = 0;
+		
+		int series_counter=1;
+		while(reader.hasNext()){
+			XMLEvent readerEvent = reader.nextEvent();
+			if(readerEvent.isStartDocument()){
+				xmlw.writeStartDocument("UTF-8", "1.0");
+				xmlw.writeComment("xml-stylesheet entry omitted for debgging purpose.");
+			}else if(readerEvent.isStartElement()){
+				StartElement start = readerEvent.asStartElement();
+				
+				// Is the event now in ... element?
+				in_roi_element = false;
+				in_roi_2d_element = false;
+				
+				// String to store the value of the roi_dimensions attribute
+				String roi_dimensions = "";
+				
+				// Starting element of the document -> just write it with default namespace
+				if (start.getName().getLocalPart().equals("rpacs_processing")){
+					xmlw.writeStartElement(start.getName().getLocalPart());	
+					xmlw.writeDefaultNamespace(start.getName().getNamespaceURI());
+					
+				// Series element -> write the element and attributes
+				}else if(start.getName().getLocalPart().equals("series")){
+					file_element_counter = 0;
+					
+					// Write the element and attributes
+					xmlw.writeStartElement(start.getName().getLocalPart());	
+					for(Iterator attr_i = start.getAttributes(); attr_i.hasNext(); ){
+						Attribute attr = (Attribute) attr_i.next();
+						xmlw.writeAttribute(attr.getName().getLocalPart(), attr.getValue());
+					}
+					
+					// When it's the series of user's choice
+					if(series_counter == series_n){														
+						
+						// Write the roi_info in the series element to the XML file
+						xmlw.writeStartElement("roi_info");
+						
+						// Get creation time and date
+						Calendar cal = Calendar.getInstance();
+						String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+						SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+						
+						// Write creation time and date, creator application name, UserID of creator												
+						xmlw.writeAttribute("creation_date", roi_creation_date);
+						xmlw.writeAttribute("creator_app", application_name);						
+						xmlw.writeAttribute("creator_id", roi_creation_username);
+						
+						// See if there's already information stored about ROI of another dimension
+						// write it again with the new ROI dimension info
+						if (!roi_dimensions.isEmpty()){
+							if (roi_dimensions.equals(roi_dimension))
+								xmlw.writeAttribute("roi_dimensions", roi_dimensions);
+							else if(roi_dimensions.contains(Integer.toString(roi_dimension)))
+								xmlw.writeAttribute("roi_dimensions", roi_dimensions);
+							else
+								xmlw.writeAttribute("roi_dimensions", Integer.toString(roi_dimension));							
+						}else
+							xmlw.writeAttribute("roi_dimensions", Integer.toString(roi_dimension));
+						
+						xmlw.writeEndElement();
+						
+						
+						// For James and MRIW
+						//for(int i=0; i<boundaries_array.length; i++){
+							xmlw.writeStartElement("roi_for_mriw");
+							
+							xmlw.writeAttribute("x-size", image_size[0]);
+							xmlw.writeAttribute("y-size", image_size[1]);
+							
+							String roi_coords = "";
+							// Write the coordinates of the ROI as character element
+							//for(int j=0; j<boundaries_array[i][file_element_counter].length; j++){
+							for(int j=0; j<xs.length; j++){
+								String roi_coord;
+								//roi_coord = "(" + Double.toString(boundaries_array[i][file_element_counter][j][1]) + "," + Double.toString(boundaries_array[i][file_element_counter][j][0]) + ")";
+								roi_coord = "(" + xs[j] + "," + ys[j] + ")"; 
+								//if(boundaries_array[i][file_element_counter][j][1] != 0 && boundaries_array[i][file_element_counter][j][0] != 0){							
+									//xmlw.writeCharacters(roi_coord);
+								//	roi_coords += roi_coord;
+								//}
+								//xmlw.writeCharacters(" ");
+								roi_coords += " ";
+							}
+							xmlw.writeAttribute("coordinates", roi_coords);
+							
+							xmlw.writeEndElement();
+						//}
+						
+						
+						//series_counter++;
+						//continue;
+					}
+					series_counter++;
+					
+				// To skip writing the roi_info element again, set the in_roi_element to true.
+				// And read out the value of the roi_dimensions attribute
+				}else if(start.getName().getLocalPart().equals("roi_info") && series_n+1 == series_counter){
+					in_roi_element = true;
+					
+					for(Iterator roi_attr_i = start.getAttributes(); roi_attr_i.hasNext(); ){
+						Attribute roi_attr = (Attribute) roi_attr_i.next();
+						if(roi_attr.getName().getLocalPart().equals("roi_dimensions")){
+							roi_dimensions = roi_attr.getValue();								
+						}
+					}
+					
+				// To skip writing the roi_2d element again, set the in_roi_2d_element to true.
+				// And write the EndElement for the file element.
+				}else if(start.getName().getLocalPart().equals("roi_2d") && series_n+1 == series_counter){
+					in_roi_2d_element = true;
+					xmlw.writeEndElement();
+					
+				// In the file element.
+				}else if(start.getName().getLocalPart().equals("file") && series_n+1 == series_counter){					
+					xmlw.writeStartElement(start.getName().getLocalPart());					
+					for(Iterator attr_i = start.getAttributes(); attr_i.hasNext(); ){
+						Attribute attr = (Attribute) attr_i.next();
+						xmlw.writeAttribute(attr.getName().getLocalPart(), attr.getValue());
+					}					
+					
+					// Write the roi_2d element in the file element.
+					// As many as the number of labels of the ROI.
+					int i = 0;
+					//for(int i=0; i<boundaries_array.length; i++){
+						xmlw.writeStartElement("roi_2d");
+						xmlw.writeAttribute("label", Integer.toString(i));
+						
+						String roi_coords = "";
+						// Write the coordinates of the ROI as character element
+						//for(int j=0; j<boundaries_array[i][file_element_counter].length; j++){
+						for(int j=0; j<xs.length; j++){
+							String roi_coord;
+							//roi_coord = "(" + Double.toString(boundaries_array[i][file_element_counter][j][0]) + "," + Double.toString(boundaries_array[i][file_element_counter][j][1]) + ")";
+							roi_coord = "(" + ys[j] + "," + xs[j] + ")";
+							//if(boundaries_array[i][file_element_counter][j][0] != 0 && boundaries_array[i][file_element_counter][j][1] != 0){							
+								//xmlw.writeCharacters(roi_coord);
+							//	roi_coords += roi_coord;
+							//}
+							//xmlw.writeCharacters(" ");
+							roi_coords += " ";
+						}
+						xmlw.writeAttribute("coordinates", roi_coords);
+						
+						xmlw.writeEndElement();
+					//}
 										
 					file_element_counter++;
 				}
